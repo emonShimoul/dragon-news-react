489312e165e8ce277ca11b2dc7e909277a8922c6
emonShimoul <emonshimoul@gmail.com>
2025-02-11 02:31:22 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center font-bold text-red-600">Vite + React</h1>
+      <span className="loading loading-spinner text-primary"></span>
+      <span className="loading loading-spinner text-secondary"></span>
+      <span className="loading loading-spinner text-accent"></span>
+      <span className="loading loading-spinner text-neutral"></span>
+      <span className="loading loading-spinner text-info"></span>
+      <span className="loading loading-spinner text-success"></span>
+      <span className="loading loading-spinner text-warning"></span>
+      <span className="loading loading-spinner text-error"></span>
+    </>
+  );
+}
+
+export default App;
