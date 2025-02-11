0b3730cf2fc3d86d245dbaa4144214f38c7aded7
emonShimoul <emonshimoul@gmail.com>
2025-02-11 12:31:04 +0600

    design right navbar, use navlink on categoryBtn and understand data architechture

--- src/components/layout-component/LeftNavbar.jsx
+++ src/components/layout-component/LeftNavbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const LeftNavbar = () => {
   const [categories, setCategories] = useState([]);
@@ -13,9 +14,13 @@ const LeftNavbar = () => {
       <h2 className="font-semibold mb-4">All Category ({categories.length})</h2>
       <div className="flex flex-col gap-2">
         {categories.map((category) => (
-          <button className="btn" key={category.category_id}>
+          <NavLink
+            to={`/category/${category.category_id}`}
+            className="btn bg-base-100 border-none"
+            key={category.category_id}
+          >
             {category.category_name}
-          </button>
+          </NavLink>
         ))}
       </div>
     </div>
