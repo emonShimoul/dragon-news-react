22a501405388428d117c1aa549de649c19aba63d
emonShimoul <emonshimoul@gmail.com>
2025-02-12 19:25:19 +0600

    implement private route and redirect user after login

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -13,17 +13,21 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  console.log(user);
+  const [loading, setLoading] = useState(true);
+  //   console.log(user, loading);
 
   const createNewUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const userLogin = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
 
@@ -33,11 +37,13 @@ const AuthProvider = ({ children }) => {
     createNewUser,
     userLogin,
     logOut,
+    loading,
   };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
     return () => {
       unsubscribe();
